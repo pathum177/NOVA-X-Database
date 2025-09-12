ab53e1b76c25648a9e8c02d7fee83308255908cd
pathum177 <pathummalsara177@gmail.com>
2025-09-12 12:21:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
 
-async function connectToWA() {
+async function connectToWA() {;
 					/////////////////MONGODB.///////////////
 const connectDB = require(`./lib/mongodb`)
 connectDB();
