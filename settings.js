c87877666e3811633ee981f7389d3dc00777cb4c
pathum177 <pathummalsara177@gmail.com>
2025-09-16 00:15:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || 'KSMD~P9pH0ZSS#hDshLEPLcjzJOvFH8O9gUMElulPz-ywsqOxzTL6VTeQ',
+SESSION_ID: process.env.SESSION_ID || '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=1518314290523893',
 MONGODB: process.env.MONGODB || "mongodb://mongo:hSKOyytRSHAikyAFUajcmkoJTEcgoBTR@trolley.proxy.rlwy.net:29757",  
 PREFIX: process.env.PREFIX || '.',
 ALIVE_IMG: process.env.ALIVE_IMG || 'https://files.catbox.moe/er0vnl.png',   
